78b75820a7d4ddd91b79f55769f7304f453024d9
Lorenzo Califano <lorenzo.califano@paasolutions.it>
2026-05-09 16:32:34 +0000

    rimozione test

--- test/ProductService.test.js
+++ test/ProductService.test.js
@@ -1,15 +1,10 @@
 const cds = require('@sap/cds')
 
 const { GET, POST, expect, axios } = cds.test (__dirname+'/..')
-axios.defaults.auth = { username: 'alice', password: '' }
+axios.defaults.auth = { username: 'alice', password: 'alice' }
 
 describe('ProductService OData APIs', () => {
 
-  it('serves ProductService.Product', async () => {
-    const { data } = await GET `/odata/v4/api/Product ${{ params: { $select: 'ID,name' } }}`
-    expect(data.value).to.containSubset([
-      {"ID":"71064963-43ac-4458-a540-1c795419da02","name":"name-710649"},
-    ])
-  })
+  
 
 })
